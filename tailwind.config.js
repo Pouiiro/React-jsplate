09186f2fcd3b65257d727ebd91b65e8f24d23947
aimdexter <draimade@gmail.com>
2022-08-30 16:32:58 +0100

    Added support for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
