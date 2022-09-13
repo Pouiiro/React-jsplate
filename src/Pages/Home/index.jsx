45b34609f202615b273d44b0c201b29d7dfd9376
aimdexter <draimade@gmail.com>
2022-09-13 18:40:42 +0100

    Added tailwind v3 Support

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>Home Page</h1>
     </div>
   )
 }
